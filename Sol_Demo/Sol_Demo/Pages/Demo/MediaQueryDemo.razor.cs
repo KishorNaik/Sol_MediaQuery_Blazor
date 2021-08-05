feaad93443d4ef9784b002a8e656a1ee54d729fe
Kishor Naik <kishor.naik011.net@gmail.com>
2021-08-05 21:10:29 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/Demo/MediaQueryDemo.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class MediaQueryDemo
+    {
+        #region State Property
+
+        private bool IsLoading { get; set; }
+
+        #endregion State Property
+
+        #region LifeCycle
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                IsLoading = true;
+                base.StateHasChanged();
+            }
+        }
+
+        #endregion LifeCycle
+    }
+}
